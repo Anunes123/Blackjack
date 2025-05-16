ae605792a6fd58250c9b9c08dac26fa2903a3954
anunes123 <andrew_nunes26@milton.edu>
2025-05-16 13:50:53 -0400

    started

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Card {
+    public String number; //ex ace,2,3
+    public String suit;
+    public boolean isEleven; //for aces
+    public int value; // ex 11,2,3
 }
